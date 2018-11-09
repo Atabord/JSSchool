493e11638b58a2d0e49bd97bc04479766aacbcd1
Alejandro <atabord@hotmail.com>
2018-11-09 15:46:20 +0000

    copying ChallengeC03 into ChallengeC05

---
+++ ChallengeC05/js/app.js
@@ -0,0 +1,22 @@
+$(function(){
+  $('.dropdown-button').focus(function(){
+    $(this).siblings(".dropdown-list").addClass("show");
+  });
+  $(document).on('click', function(event) {
+    if (!$(event.target).closest('.dropdown-list').length) {
+      $(".dropdown-list").removeClass("show");
+    }
+  });
+  $('.book').hover(function(){
+    $(this).find(".book-hover").toggleClass("show");
+    $(this).find(".popup-book").toggleClass("show");
+  });
+
+  $('.navbar-toggler').click(function(){
+    $(this).parent().siblings(".navbar-collapse").toggleClass("show");
+  })
+
+  $('.collapse-button').click(function(){
+    $(this).next().toggle("linear");
+  })
+});
