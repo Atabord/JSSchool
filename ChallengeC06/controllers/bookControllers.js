4d2d556f617dbdaacad36de0b81d6d29335b1508
Alejandro <atabord@hotmail.com>
2018-11-19 00:00:47 +0000

    Adding security to all pages

--- ChallengeC06/controllers/bookControllers.js
+++ ChallengeC06/controllers/bookControllers.js
@@ -2,133 +2,190 @@ const Book = require('../models/books');
 const fetch = require('node-fetch');
 const jwt = require('jsonwebtoken');
 
-function ensureToken (req, res, next) {
-    const bearerHeader = req.headers["authorization"];
-    if (typeof bearerHeader !== 'undefined') {
-        const bearer = bearerHeader.split(" ");
-        const bearerToken = bearer[1];
-        req.token = bearerToken;
-        next();
-    } else {
-        res.sendStatus(403);
-    }
-}
-
+//function to get all books or all books on a bookshelf
 function getAllBooks(req, res) {
-    const bookShelf = req.query.bookShelf;
-    if (bookShelf){
-        Book.find({bookshelf: bookShelf}, (err, books) => {
-            if(err) {
-                res.status(500).send({message: `Error making the request ${err}`});
-            } else if(!books) {
-                res.status(404).send({message: `We don't have books in that bookshelf right now`})
+    jwt.verify(req.token, 'my_secret_key', (err, data) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            const bookShelf = req.query.bookShelf;
+            if (bookShelf) {
+                Book.find({
+                    bookshelf: bookShelf
+                }, (err, books) => {
+                    if (err) {
+                        res.status(500).send({
+                            message: `Error making the request ${err}`
+                        });
+                    } else if (!books) {
+                        res.status(404).send({
+                            message: `We don't have books in that bookshelf right now`
+                        })
+                    } else {
+                        res.status(200).send({
+                            books
+                        });
+                    }
+                })
             } else {
-                res.status(200).send({ books });
+                Book.find({}, (err, books) => {
+                    if (err) {
+                        res.status(500).send({
+                            message: `Error making the request ${err}`
+                        });
+                    } else if (!books) {
+                        res.status(404).send({
+                            message: `We don't have books right now`
+                        })
+                    } else {
+                        res.status(200).send({
+                            books
+                        });
+                    }
+                })
             }
-        })    
-    } else {
-        Book.find({}, (err, books) => {
-            if(err) {
-                res.status(500).send({message: `Error making the request ${err}`});
-            } else if(!books) {
-                res.status(404).send({message: `We don't have books right now`})
-            } else {
-                res.status(200).send({ books });
-            }
-        })
-    }
+        }
+    })
 }
 
+//function to get one book
 function getOneBook(req, res) {
-    let bookId = req.params.book;
-    Book.findById(bookId, (err, book) => {
-        if(err) {
-            res.status(500).send({message: `Error making the request ${err}`});
-        } 
-        else if(!book) {
-            res.status(404).send({message: `That book doesn't exist`})
+    jwt.verify(req.token, 'my_secret_key', (err, data) => {
+        if (err) {
+            res.sendStatus(403);
         } else {
-            res.status(200).send({ book })
-        }
-    });
-}
-
-function createBook(req, res) {
-    const isbn = req.body.isbn;
-    const place = req.body.place; //could be Quito, Cartagena, Medellín, Digital, Personal Loans
-    const copies = req.body.copies; //number of copies
-    const url = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`
-    console.log(url);
-    
-    // saving the book information of place, copies and availables
-    let book = new Book();
-    book.bookshelf = place;
-    book.copies = copies;
-    book.availableCopies = copies; // its default is the number of copies
-
-    // function to get the book information from google Api
-    async function getBook(url) {
-        try {
-            const content = await fetch(url)
-                .then(async res => res.json());
-            console.log(content.items[0].volumeInfo.title)
-            book.title = content.items[0].volumeInfo.title;
-            book.authors = content.items[0].volumeInfo.authors;
-            book.publisher = content.items[0].volumeInfo.publisher;
-            book.publishedDate = content.items[0].volumeInfo.publishedDate;
-            book.description = content.items[0].volumeInfo.description;
-            book.isbn_13 = content.items[0].volumeInfo.industryIdentifiers[0].identifier;
-            book.isbn_10 = content.items[0].volumeInfo.industryIdentifiers[1].identifier;
-            book.pageCount = content.items[0].volumeInfo.pageCount;
-            book.printType = content.items[0].volumeInfo.printType;
-            book.categories = content.items[0].volumeInfo.categories;
-            book.averageRating = content.items[0].volumeInfo.averageRating;
-            book.imageLink = content.items[0].volumeInfo.imageLinks[1];
-            book.language = content.items[0].volumeInfo.language;
-            
-            book.save((err, bookStored) => {
-                if(err) {
-                    res.status(500)
-                        .send({message: `Error saving the book ${err}`});
+            let bookId = req.params.book;
+            Book.findById(bookId, (err, book) => {
+                if (err) {
+                    res.status(500).send({
+                        message: `Error making the request ${err}`
+                    });
+                } else if (!book) {
+                    res.status(404).send({
+                        message: `That book doesn't exist`
+                    })
                 } else {
-                    res.status(200)
-                        .send({book: bookStored});
+                    res.status(200).send({
+                        book
+                    })
                 }
-            })
-        } catch(err){
-            res.status(500)
-                .send({message: `Error making the request ${err}`});
-        }
-    }
-    getBook(url);
-}
-
-function lendBook(req, res) {
-    let bookId = req.params.book;
-    Book.findById(bookId, (err, book) => {
-        if(err) {
-            res.status(500).send({message: `Error making the request ${err}`});
-        } 
-        else if(!book) {
-            res.status(404).send({message: `That book doesn't exist`});
-        } else if(book.availableCopies <= 0) {
-            if(book.bookshelf === 'Digital'){
-                res.status(200).send({message: `You have rented the book ${book.title}`});
-            } else {
-                res.status(404).send({ message: `There aren't enought available books: ${book.availableCopies}` });
-            }
-        } else {
-            // just rent them if there are available books
-            Book.findOneAndUpdate({_id: bookId}, {$inc: {availableCopies: -1}}, (err, bookUpdated) => {
-                if(err) {
-                    res.status(500).send({message: `Error making the request ${err}`});
-                } 
-                else {
-                        res.status(200).send({message: `You have rented the book ${bookUpdated.title}`});                
-                } 
             });
         }
-    });
+    })
+}
+
+//function to createbooks
+function createBook(req, res) {
+    jwt.verify(req.token, 'my_secret_key', (err, data) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            const isbn = req.body.isbn;
+            const place = req.body.place; //could be Quito, Cartagena, Medellín, Digital, Personal Loans
+            const copies = req.body.copies; //number of copies
+            const url = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`
+            console.log(url);
+
+            // saving the book information of place, copies and availables
+            let book = new Book();
+            book.bookshelf = place;
+            book.copies = copies;
+            book.availableCopies = copies; // its default is the number of copies
+
+            // function to get the book information from google Api
+            async function getBook(url) {
+                try {
+                    const content = await fetch(url)
+                        .then(async res => res.json());
+                    console.log(content.items[0].volumeInfo.title)
+                    book.title = content.items[0].volumeInfo.title;
+                    book.authors = content.items[0].volumeInfo.authors;
+                    book.publisher = content.items[0].volumeInfo.publisher;
+                    book.publishedDate = content.items[0].volumeInfo.publishedDate;
+                    book.description = content.items[0].volumeInfo.description;
+                    book.isbn_13 = content.items[0].volumeInfo.industryIdentifiers[0].identifier;
+                    book.isbn_10 = content.items[0].volumeInfo.industryIdentifiers[1].identifier;
+                    book.pageCount = content.items[0].volumeInfo.pageCount;
+                    book.printType = content.items[0].volumeInfo.printType;
+                    book.categories = content.items[0].volumeInfo.categories;
+                    book.averageRating = content.items[0].volumeInfo.averageRating;
+                    book.imageLink = content.items[0].volumeInfo.imageLinks[1];
+                    book.language = content.items[0].volumeInfo.language;
+
+                    book.save((err, bookStored) => {
+                        if (err) {
+                            res.status(500)
+                                .send({
+                                    message: `Error saving the book ${err}`
+                                });
+                        } else {
+                            res.status(200)
+                                .send({
+                                    book: bookStored
+                                });
+                        }
+                    })
+                } catch (err) {
+                    res.status(500)
+                        .send({
+                            message: `Error making the request ${err}`
+                        });
+                }
+            }
+            getBook(url);
+        }
+    })
+}
+
+//function to lend a book
+function lendBook(req, res) {
+    jwt.verify(req.token, 'my_secret_key', (err, data) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            let bookId = req.params.book;
+            Book.findById(bookId, (err, book) => {
+                if (err) {
+                    res.status(500).send({
+                        message: `Error making the request ${err}`
+                    });
+                } else if (!book) {
+                    res.status(404).send({
+                        message: `That book doesn't exist`
+                    });
+                } else if (book.availableCopies <= 0) {
+                    if (book.bookshelf === 'Digital') {
+                        res.status(200).send({
+                            message: `You have rented the book ${book.title}`
+                        });
+                    } else {
+                        res.status(404).send({
+                            message: `There aren't enought available books: ${book.availableCopies}`
+                        });
+                    }
+                } else {
+                    // just rent them if there are available books
+                    Book.findOneAndUpdate({
+                        _id: bookId
+                    }, {
+                        $inc: {
+                            availableCopies: -1
+                        }
+                    }, (err, bookUpdated) => {
+                        if (err) {
+                            res.status(500).send({
+                                message: `Error making the request ${err}`
+                            });
+                        } else {
+                            res.status(200).send({
+                                message: `You have rented the book ${bookUpdated.title}`
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    })
 }
 
 module.exports = {
--- ChallengeC06/routes/books.js
+++ ChallengeC06/routes/books.js
@@ -3,16 +3,28 @@ const express = require('express');
 const books = express.Router();
 const bookControllers = require('../controllers/bookControllers');
 
+function ensureToken (req, res, next) {
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
+
 // post to create a new book
-books.post('/new', bookControllers.createBook);
+books.post('/new', ensureToken, bookControllers.createBook);
 
 // endpoint to get all the books or all the books on a bookshelf
-books.get('/', bookControllers.getAllBooks);
+books.get('/', ensureToken, bookControllers.getAllBooks);
 
 // endpoint to get 1 specific book
-books.get('/:book', bookControllers.getOneBook);
+books.get('/:book', ensureToken, bookControllers.getOneBook);
 
 // endpoint to lend an specific book
-books.patch('/:book/lend', bookControllers.lendBook);
+books.patch('/:book/lend', ensureToken, bookControllers.lendBook);
 
 module.exports = books;
