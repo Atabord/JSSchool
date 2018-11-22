de7e795c0304bf31756017609145cedaa57229cb
Alejandro <atabord@hotmail.com>
2018-11-22 15:30:11 +0000

    Adding base structure of C07

---
+++ ChallengeC07/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    mode: 'development',
+    entry: '.\\src\\js\\index.js',
+    output: {
+      filename: 'boundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      ],
+    },
+  };
+  
